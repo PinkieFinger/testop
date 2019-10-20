cfc56dfad62ce20308aadaf6e413b7835c62b809
PinkieFinger <56165584+PinkieFinger@users.noreply.github.com>
2019-10-20 12:23:44 +0800

    Add files via upload

---
+++ allkeypoints_from_image.cpp
@@ -0,0 +1,43 @@
+#include "stdafx.h"
+#include <opencv2/opencv.hpp>
+#define OPENPOSE_FLAGS_DISABLE_POSE
+#include <openpose/headers.hpp>
+class image_to_coordinate
+{
+public:
+	std::string coordinate_body;
+	std::string coordinate_Lhand;
+	std::string coordinate_Rhand;
+	std::string coordinate_face;
+	cv::Mat cvImageToProcess;
+};
+_declspec(dllexport) int tutorialApiCpp(image_to_coordinate &tempa,op::Wrapper &opWrapper)
+{
+	try
+	{
+		op::opLog("Starting OpenPose demo...", op::Priority::High);
+		op::opLog("Configuring OpenPose...", op::Priority::High);
+		const op::Matrix imageToProcess = op::Matrix((const void* const)&(tempa.cvImageToProcess));
+		auto datumProcessed = opWrapper.emplaceAndPop(imageToProcess);
+		if (datumProcessed != nullptr)
+		{
+			tempa.coordinate_body = datumProcessed->at(0)->poseKeypoints.toString();
+			tempa.coordinate_face = datumProcessed->at(0)->faceKeypoints.toString();
+			tempa.coordinate_Lhand = datumProcessed->at(0)->handKeypoints[0].toString();
+			tempa.coordinate_Rhand = datumProcessed->at(0)->handKeypoints[1].toString();
+			tempa.coordinate_body.erase(0, 22);
+			tempa.coordinate_face.erase(0, 22);
+			tempa.coordinate_Lhand.erase(0, 22);
+			tempa.coordinate_Rhand.erase(0, 22);
+		}
+		else
+			op::opLog("Image could not be processed.", op::Priority::High);
+		// Measuring total time
+		// Return
+		return 0;
+	}
+	catch (const std::exception&)
+	{
+		return -1;
+	}
+}
---
+++ trytry.cpp
@@ -0,0 +1,48 @@
+// trytry.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#pragma comment(lib,"allkeypoints_from_image.lib")
+#pragma comment(lib,"opencv_world411.lib")
+#pragma comment(lib,"opencv_world411d.lib")
+
+
+int tutorialApiCpp(image_to_coordinate &tempa,op::Wrapper &opWrapper);
+
+int main()
+{
+	image_to_coordinate tempa;
+	op::Wrapper opWrapper{ op::ThreadManagerMode::Asynchronous };
+	// Add hand and face
+	opWrapper.configure(op::WrapperStructFace{ false });
+	opWrapper.configure(op::WrapperStructHand{ false });
+	opWrapper.disableMultiThreading();
+	opWrapper.start();
+	cv::VideoCapture capture(0);
+	while (true)
+	{
+		//const auto opTimer = op::getTimerInit();
+		cv::Mat frame;
+		capture >> frame;
+		tempa.cvImageToProcess = frame;
+		const op::Matrix imageToProcess = op::Matrix((const void* const)&(tempa.cvImageToProcess));
+		tutorialApiCpp(tempa, opWrapper);
+		const cv::Mat cvMat = (*((cv::Mat*)((opWrapper.emplaceAndPop(imageToProcess)->at(0)->cvOutputData).getConstCvMat())));
+		imshow("读取视频", cvMat);
+		op::opLog(tempa.coordinate_body, op::Priority::High);
+		cv::waitKey(30);	//延时30
+		//op::printTime(opTimer, "OpenPose demo successfully finished. Total time: ", " seconds.", op::Priority::High);
+	}
+
+	//image_to_coordinate tempa;
+	//tempa.cvImageToProcess = (cv::imread("examples/media/COCO_val2014_000000000294.jpg"));
+	//tutorialApiCpp(tempa);
+	//op::opLog(tempa.coordinate_body, op::Priority::High);
+	//op::opLog(tempa.coordinate_face, op::Priority::High);
+	//op::opLog(tempa.coordinate_Lhand, op::Priority::High);
+	//op::opLog(tempa.coordinate_Rhand, op::Priority::High);
+
+	while (1);
+	return 0;
+}
+
